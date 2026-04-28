88542b342f6aa6bf984512ec8614c103edfb7826
Kaden-Beck <kadenb12@gmail.com>
2026-04-28 14:01:06 -0600

    Removed starter filles Changes how google fonts were imported

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import ThemeRegistry from "@/components/ThemeRegistry";
-import "./globals.css";
+
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400", "500", "700"] });
 
 export const metadata: Metadata = {
     title: "kaden-beck.github.io",
@@ -14,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={roboto.className}>
             <body>
                 <AppRouterCacheProvider>
                     <ThemeRegistry>{children}</ThemeRegistry>
